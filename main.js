3affa3470f825a88284ba2157d9ab31c94333eaf
Alan <alan88w@yahoo.com>
2018-12-15 14:08:16 +0800

    initial commit - not complete yet

---
+++ main.js
@@ -0,0 +1,11 @@
+function myTopBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+document.getElementsByClassName("icon")[0].addEventListener('click', myTopBar);
